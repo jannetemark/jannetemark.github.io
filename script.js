b22de83715ed5076b02aedcddcf50a327f0e8707
jannetemark <62158703+jannetemark@users.noreply.github.com>
2020-04-07 23:49:42 +0200

    new website

---
+++ script.js
@@ -0,0 +1,22 @@
+// see works.js for list of works
+const randomWork = works[Math.floor(Math.random() * works.length)];
+const work = document.querySelector(".work");
+const workImage = new Image();
+const workDescription = document.querySelector(".work .description");
+workImage.src = `images/works/${randomWork.filename}`;
+workImage.alt = workDescription.innerText = randomWork.title;
+work.appendChild(workImage);
+
+const contact = document.querySelector(".contact");
+contact.addEventListener("mouseenter", (event) => {
+  const img = event.currentTarget.querySelector("img");
+  img.src = img.dataset.hover;
+});
+contact.addEventListener("mouseleave", (event) => {
+  const img = event.currentTarget.querySelector("img");
+  img.src = img.dataset.src;
+});
+
+setTimeout(() => {
+  alert("So... what are you waiting for?");
+}, 60000);
---
+++ works.js
@@ -0,0 +1,154 @@
+const works = [
+  {
+    title: `"The Factory" Carlos Amorales, book in collaboration with Mevis & Van Deursen, 2019`,
+    filename: `the_factory_carlos_amorales_book_in_collaboration_with_mevis_van_deursen_2019_1.jpg`,
+  },
+  {
+    title: `"The Factory" Carlos Amorales, book in collaboration with Mevis & Van Deursen, 2019`,
+    filename: `the_factory_carlos_amorales_book_in_collaboration_with_mevis_van_deursen_2019_2.jpg`,
+  },
+  {
+    title: `"The Factory" Carlos Amorales, book in collaboration with Mevis & Van Deursen, 2019`,
+    filename: `the_factory_carlos_amorales_book_in_collaboration_with_mevis_van_deursen_2019_3.jpg`,
+  },
+  {
+    title: `"The Factory" Carlos Amorales, book in collaboration with Mevis & Van Deursen, 2019`,
+    filename: `the_factory_carlos_amorales_book_in_collaboration_with_mevis_van_deursen_2019_4.jpg`,
+  },
+  {
+    title: `Auguries of Innocence, poster, 2018`,
+    filename: `auguries_of_innocenceposter_2018.jpg`,
+  },
+  {
+    title: `Beyond the figure, poster, 2019`,
+    filename: `beyond_the_figure_poster_2019.jpg`,
+  },
+  {
+    title: `Body of Avant-garde, poster, 2016`,
+    filename: `body_of_avant-garde_poster_2016.jpg`,
+  },
+  {
+    title: `Darkness memories I, poster, 2018`,
+    filename: `darkness_memories_i_poster_2018.jpg`,
+  },
+  {
+    title: `Darkness memories II, poster, 2018`,
+    filename: `darkness_memories_ii_poster_2018.jpg`,
+  },
+  {
+    title: `Darkness memories III, poster, 2018`,
+    filename: `darkness_memories_iii_poster_2018.jpg`,
+  },
+  {
+    title: `Darkness memories IV, poster, 2018`,
+    filename: `darkness_memories_iv_poster_2018.jpg`,
+  },
+  {
+    title: `Documentation of stones, 2019`,
+    filename: `documentation_of_stones_2019.jpg`,
+  },
+  {
+    title: `Dream, poster, 2017`,
+    filename: `dream_poster_2017.jpg`,
+  },
+  {
+    title: `El descriptar de lo buscar, I, book, 2020`,
+    filename: `el_descriptar_de_lo_buscari_book_2020.jpg`,
+  },
+  {
+    title: `El descriptar de lo buscar, II, book, 2020`,
+    filename: `el_descriptar_de_lo_buscarii_book_2020.jpg`,
+  },
+  {
+    title: `El descriptar de lo buscar, III, book, 2020`,
+    filename: `el_descriptar_de_lo_buscariii_book_2020.jpg`,
+  },
+  {
+    title: `Fantasma, performance at kurimanzutto, 2020`,
+    filename: `fantasma_performance_at_kurimanzutto_2020.jpg`,
+  },
+  {
+    title: `Found image, CDMX, 2019`,
+    filename: `found_image_cdmx_2019.jpg`,
+  },
+  {
+    title: `How we bring it together, publication/performance, 2019`,
+    filename: `how_we_bring_it_togetherpublication_performance_2019.JPG`,
+  },
+  {
+    title: `I've got you, poster, 2017`,
+    filename: `ive_got_you_poster_2017.jpg`,
+  },
+  {
+    title: `Invisible landscape, book, 2019`,
+    filename: `invisible_landscapebook2019.jpg`,
+  },
+  {
+    title: `Light, poster, 2017`,
+    filename: `light_poster_2017.jpg`,
+  },
+  {
+    title: `Prepare yourself, poster, 2019,`,
+    filename: `prepare_yourselfposter_2019.jpg`,
+  },
+  {
+    title: `Reality, poster, 2017`,
+    filename: `reality_poster_2017.jpg`,
+  },
+  {
+    title: `Red rock, poster, 2019`,
+    filename: `red_rock_poster_2019.jpg`,
+  },
+  {
+    title: `Territories, poster,2019`,
+    filename: `territoriesposter2019.jpg`,
+  },
+  {
+    title: `Time passing, poster/radio performance, 2019`,
+    filename: `time_passingposter_radio_performance_2019.jpg`,
+  },
+  {
+    title: `Under the volcano series, I, 2019`,
+    filename: `under_the_volcano_series_i_2019.jpg`,
+  },
+  {
+    title: `Under the volcano series, II, 2019`,
+    filename: `under_the_volcano_series_ii_2019.jpg`,
+  },
+  {
+    title: `Universe, poster, 2017`,
+    filename: `universe_poster_2017.jpg`,
+  },
+  {
+    title: `Volcano maps, 2018`,
+    filename: `volcano_maps_2018.jpg`,
+  },
+  {
+    title: `Volcano maps, 2019`,
+    filename: `volcano_maps_2019.jpg`,
+  },
+  {
+    title: `Volcano maps, I publication, 2020`,
+    filename: `volcano_maps_i_publication_2020.jpg`,
+  },
+  {
+    title: `Volcano maps, II publication, 2020`,
+    filename: `volcano_maps_ii_publication_2020.jpg`,
+  },
+  {
+    title: `Volcano maps, III publication, 2020`,
+    filename: `volcano_maps_iii_publication_2020.jpg`,
+  },
+  {
+    title: `Volcano maps, IV publication, 2020`,
+    filename: `volcano_maps_iv_publication_2020.jpg`,
+  },
+  {
+    title: `Volcano maps, publication, 2020`,
+    filename: `volcano_maps_publication_2020.jpg`,
+  },
+  {
+    title: `Volcano poetry, publication,2020`,
+    filename: `volcano_poetry_publication2020.jpg`,
+  },
+];
