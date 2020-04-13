af18bb678b8d6bc5ce0fde662bb52f1074de825d
jannetemark <62158703+jannetemark@users.noreply.github.com>
2020-04-13 13:03:00 +0200

    New work

--- works.js
+++ works.js
@@ -4,43 +4,43 @@ const works = [
     filename: `the_factory_carlos_amorales_book_in_collaboration_with_mevis_van_deursen_2019_1.jpg`,
   },
   {
-    title: `"The Factory" Carlos Amorales, book in collaboration with Mevis & Van Deursen, 2019`,
+    title: `"The Factory" Carlos Amorales, Stedelijk Amsterdam book in collaboration with Mevis & Van Deursen, 2019`,
     filename: `the_factory_carlos_amorales_book_in_collaboration_with_mevis_van_deursen_2019_2.jpg`,
   },
   {
-    title: `"The Factory" Carlos Amorales, book in collaboration with Mevis & Van Deursen, 2019`,
+    title: `"The Factory" Carlos Amorales, Stedelijk Amsterdam book in collaboration with Mevis & Van Deursen, 2019`,
     filename: `the_factory_carlos_amorales_book_in_collaboration_with_mevis_van_deursen_2019_3.jpg`,
   },
   {
-    title: `"The Factory" Carlos Amorales, book in collaboration with Mevis & Van Deursen, 2019`,
+    title: `"The Factory" Carlos Amorales, Stedelijk Amsterdam book in collaboration with Mevis & Van Deursen, 2019`,
     filename: `the_factory_carlos_amorales_book_in_collaboration_with_mevis_van_deursen_2019_4.jpg`,
   },
   {
-    title: `Auguries of Innocence, poster, 2018`,
+    title: `Auguries of Innocence, poster, NYABF, 2018`,
     filename: `auguries_of_innocenceposter_2018.jpg`,
   },
   {
-    title: `Beyond the figure, poster, 2019`,
+    title: `Beyond the figure, poster, Learning Community, 2019`,
     filename: `beyond_the_figure_poster_2019.jpg`,
   },
   {
-    title: `Body of Avant-garde, poster, 2016`,
+    title: `Body of Avant-garde, poster, Meyerhold Theatre, 2016`,
     filename: `body_of_avant-garde_poster_2016.jpg`,
   },
   {
-    title: `Darkness memories I, poster, 2018`,
+    title: `Darkness memories, poster, 2018`,
     filename: `darkness_memories_i_poster_2018.jpg`,
   },
   {
-    title: `Darkness memories II, poster, 2018`,
+    title: `Darkness memories, poster, 2018`,
     filename: `darkness_memories_ii_poster_2018.jpg`,
   },
   {
-    title: `Darkness memories III, poster, 2018`,
+    title: `Darkness memories, poster, 2018`,
     filename: `darkness_memories_iii_poster_2018.jpg`,
   },
   {
-    title: `Darkness memories IV, poster, 2018`,
+    title: `Darkness memories, poster, 2018`,
     filename: `darkness_memories_iv_poster_2018.jpg`,
   },
   {
@@ -52,19 +52,19 @@ const works = [
     filename: `dream_poster_2017.jpg`,
   },
   {
-    title: `El descriptar de lo buscar, I, book, 2020`,
+    title: `"El descriptar de lo buscar" in collaboration with Iván Martínez, book, CDMX, 2020`,
     filename: `el_descriptar_de_lo_buscari_book_2020.jpg`,
   },
   {
-    title: `El descriptar de lo buscar, II, book, 2020`,
+    title: `"El descriptar de lo buscar" in collaboration with Iván Martínez, book, CDMX, 2020`,
     filename: `el_descriptar_de_lo_buscarii_book_2020.jpg`,
   },
   {
-    title: `El descriptar de lo buscar, III, book, 2020`,
+    title: `El descriptar de lo buscar" in collaboration with Iván Martínez, book, CDMX, 2020`,
     filename: `el_descriptar_de_lo_buscariii_book_2020.jpg`,
   },
   {
-    title: `Fantasma, performance at kurimanzutto, 2020`,
+    title: `"Fantasma" project with Iván Martínez in collaboration with Enrique Arriaga, performance at kurimanzutto, 2020`,
     filename: `fantasma_performance_at_kurimanzutto_2020.jpg`,
   },
   {
@@ -72,7 +72,7 @@ const works = [
     filename: `found_image_cdmx_2019.jpg`,
   },
   {
-    title: `How we bring it together, publication/performance, 2019`,
+    title: `"How we bring it together" in collaboration with dancer Sofia Kondylia, publication/performance, 2019`,
     filename: `how_we_bring_it_togetherpublication_performance_2019.JPG`,
   },
   {
@@ -104,15 +104,15 @@ const works = [
     filename: `territoriesposter2019.jpg`,
   },
   {
-    title: `Time passing, poster/radio performance, 2019`,
+    title: `"Time passing", poster/phantom radio performance, 2019`,
     filename: `time_passingposter_radio_performance_2019.jpg`,
   },
   {
-    title: `Under the volcano series, I, 2019`,
+    title: `Under the volcano series, 2019`,
     filename: `under_the_volcano_series_i_2019.jpg`,
   },
   {
-    title: `Under the volcano series, II, 2019`,
+    title: `Under the volcano series, 2019`,
     filename: `under_the_volcano_series_ii_2019.jpg`,
   },
   {
@@ -120,27 +120,27 @@ const works = [
     filename: `universe_poster_2017.jpg`,
   },
   {
-    title: `Volcano maps, 2018`,
+    title: `Volcano maps, publication, 2018`,
     filename: `volcano_maps_2018.jpg`,
   },
   {
-    title: `Volcano maps, 2019`,
+    title: `Volcano maps, publication, 2019`,
     filename: `volcano_maps_2019.jpg`,
   },
   {
-    title: `Volcano maps, I publication, 2020`,
+    title: `Volcano maps, publication, 2020`,
     filename: `volcano_maps_i_publication_2020.jpg`,
   },
   {
-    title: `Volcano maps, II publication, 2020`,
+    title: `Volcano maps, publication, 2020`,
     filename: `volcano_maps_ii_publication_2020.jpg`,
   },
   {
-    title: `Volcano maps, III publication, 2020`,
+    title: `Volcano maps, publication, 2020`,
     filename: `volcano_maps_iii_publication_2020.jpg`,
   },
   {
-    title: `Volcano maps, IV publication, 2020`,
+    title: `Volcano maps, publication, 2020`,
     filename: `volcano_maps_iv_publication_2020.jpg`,
   },
   {
@@ -150,5 +150,9 @@ const works = [
   {
     title: `Volcano poetry, publication, 2020`,
     filename: `volcano_poetry_publication2020.jpg`,
+  },
+   {
+    title: `Eternal Love Triangle, poster, WTxSandberg, 2019`,
+    filename: `eternal_love_triangle_poster_2019.jpg`,
   },
 ];
