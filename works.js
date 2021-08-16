7ba0626062ef986661dcc5ac28a31e3f0836cecc
jannetemark <62158703+jannetemark@users.noreply.github.com>
2021-08-16 19:08:04 +0300

    Update works

--- works.js
+++ works.js
@@ -1,26 +1,14 @@
 const works = [
   {
-    title: `"The Factory" Carlos Amorales, book in collaboration with Mevis & Van Deursen, 2019`,
-    filename: `the_factory_carlos_amorales_book_in_collaboration_with_mevis_van_deursen_2019_1.jpg`,
-  },
-  {
-    title: `"The Factory" Carlos Amorales, Stedelijk Amsterdam book in collaboration with Mevis & Van Deursen, 2019`,
-    filename: `the_factory_carlos_amorales_book_in_collaboration_with_mevis_van_deursen_2019_2.jpg`,
-  },
-  {
-    title: `"The Factory" Carlos Amorales, Stedelijk Amsterdam book in collaboration with Mevis & Van Deursen, 2019`,
-    filename: `the_factory_carlos_amorales_book_in_collaboration_with_mevis_van_deursen_2019_3.jpg`,
-  },
-  {
     title: `"The Factory" Carlos Amorales, Stedelijk Amsterdam book in collaboration with Mevis & Van Deursen, 2019`,
     filename: `the_factory_carlos_amorales_book_in_collaboration_with_mevis_van_deursen_2019_4.jpg`,
   },
   {
-    title: `Auguries of Innocence, poster, NYABF, 2018`,
+    title: `Auguries of Innocence, poster, NYABF, NYC, 2018`,
     filename: `auguries_of_innocenceposter_2018.jpg`,
   },
   {
-    title: `Beyond the figure, poster, Learning Community, 2019`,
+    title: `Beyond the figure, poster, Learning Community workshop, 2019`,
     filename: `beyond_the_figure_poster_2019.jpg`,
   },
   {
@@ -52,36 +40,12 @@ const works = [
     filename: `dream_poster_2017.jpg`,
   },
   {
-    title: `"El descriptar de lo buscar" in collaboration with Iván Martínez, book, CDMX, 2020`,
-    filename: `el_descriptar_de_lo_buscari_book_2020.jpg`,
-  },
-  {
-    title: `"El descriptar de lo buscar" in collaboration with Iván Martínez, book, CDMX, 2020`,
-    filename: `el_descriptar_de_lo_buscarii_book_2020.jpg`,
-  },
-  {
-    title: `El descriptar de lo buscar" in collaboration with Iván Martínez, book, CDMX, 2020`,
-    filename: `el_descriptar_de_lo_buscariii_book_2020.jpg`,
-  },
-  {
-    title: `"Fantasma" project with Iván Martínez in collaboration with Enrique Arriaga, performance at kurimanzutto, 2020`,
+    title: `"Fantasma" performance in collaboration with Iván Martínez, Enrique Arriaga at Kurimanzutto Gallery, CDMX, 2020`,
     filename: `fantasma_performance_at_kurimanzutto_2020.jpg`,
-  },
-  {
-    title: `Found image, CDMX, 2019`,
-    filename: `found_image_cdmx_2019.jpg`,
-  },
-  {
-    title: `"How we bring it together" in collaboration with dancer Sofia Kondylia, publication/performance, 2019`,
-    filename: `how_we_bring_it_togetherpublication_performance_2019.JPG`,
   },
   {
     title: `I've got you, poster, 2017`,
     filename: `ive_got_you_poster_2017.jpg`,
-  },
-  {
-    title: `Invisible landscape, book, 2019`,
-    filename: `invisible_landscapebook2019.jpg`,
   },
   {
     title: `Light, poster, 2017`,
@@ -96,63 +60,59 @@ const works = [
     filename: `reality_poster_2017.jpg`,
   },
   {
-    title: `Red rock, poster, 2019`,
+    title: `Red rock, poster, the Netherlands, 2019`,
     filename: `red_rock_poster_2019.jpg`,
   },
   {
-    title: `Territories, poster, 2019`,
+    title: `Territories, poster, the Netherlands, 2019`,
     filename: `territoriesposter2019.jpg`,
   },
   {
-    title: `"Time passing", poster/phantom radio performance, 2019`,
+    title: `"Time passing", poster for the Phantom Radio performance, the Netherlands, 2019`,
     filename: `time_passingposter_radio_performance_2019.jpg`,
   },
   {
-    title: `Under the volcano series, 2019`,
+    title: `Under the volcano series, produced by FalafelBooks, Moscow, 2019`,
     filename: `under_the_volcano_series_i_2019.jpg`,
   },
   {
-    title: `Under the volcano series, 2019`,
+    title: `Under the volcano series, produced by FalafelBooks, Moscow, 2019`,
     filename: `under_the_volcano_series_ii_2019.jpg`,
   },
   {
-    title: `Universe, poster, 2017`,
+    title: `Universe, poster, Hackers&Designers, Amsterdam, 2017`,
     filename: `universe_poster_2017.jpg`,
   },
   {
-    title: `Volcano maps, publication, 2018`,
+    title: `Volcano Maps, publication, 2018`,
     filename: `volcano_maps_2018.jpg`,
   },
   {
-    title: `Volcano maps, publication, 2019`,
+    title: `Volcano Maps, publication, 2019`,
     filename: `volcano_maps_2019.jpg`,
   },
   {
-    title: `Volcano maps, publication, 2020`,
+    title: `Volcano Maps, publication, 2020`,
     filename: `volcano_maps_i_publication_2020.jpg`,
   },
   {
-    title: `Volcano maps, publication, 2020`,
+    title: `Volcano Maps, publication, 2020`,
     filename: `volcano_maps_ii_publication_2020.jpg`,
   },
   {
-    title: `Volcano maps, publication, 2020`,
+    title: `Volcano Maps, publication, 2020`,
     filename: `volcano_maps_iii_publication_2020.jpg`,
   },
   {
-    title: `Volcano maps, publication, 2020`,
+    title: `Volcano Maps, publication, 2020`,
     filename: `volcano_maps_iv_publication_2020.jpg`,
   },
   {
-    title: `Volcano maps, publication, 2020`,
+    title: `Volcano Maps, publication, 2020`,
     filename: `volcano_maps_publication_2020.jpg`,
   },
-  {
-    title: `Volcano poetry, publication, 2020`,
-    filename: `volcano_poetry_publication2020.jpg`,
-  },
    {
-    title: `Eternal Love Triangle, poster, WTxSandberg, 2019`,
+    title: `Eternal Love Triangle, poster, Werkplaats TypografiexSandberg Institute, the Netherlands, 2019`,
     filename: `eternal_love_triangle_poster_2019.jpg`,
   },
 ];
